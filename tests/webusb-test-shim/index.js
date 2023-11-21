58db658b03bb38a30b32f82ed84979bad57c0718
Ingvar Stepanyan <me@rreverser.com>
2023-11-21 19:51:25 +0000

    Commit test shim

---
+++ tests/webusb-test-shim/index.js
@@ -0,0 +1,12 @@
+// It's not yet possible to automate actual Chrome's device selection, so
+// for now run automated tests via Node.js WebUSB implementation.
+//
+// It might differ from browser one, but should be enough to catch most obvious issues.
+
+const { usb, webusb } = require('usb');
+
+usb.setDebugLevel(3);
+
+globalThis.navigator = {
+  usb: webusb
+};
